79581ecad3be64734d7e2081301ac716a52e1549
IBRAHIMA <hadi.ibrahima8@gmail.com>
2022-04-29 12:34:32 +0100

    Update step4

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(name:"Admin",
-           email:'hadi.ibrahima77gmail.com',
+           email:'hadi.ibrahima77@gmail.com',
         password:'firstadmin123456',
            admin:true)
   @tasks=Task.where(user_id:nil)
